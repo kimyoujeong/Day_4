9ee61f5b1e7478c1830157f7a51f07fb40bcc585
kimyoujeong <55773434+kimyoujeong@users.noreply.github.com>
2021-01-02 14:19:03 +0900

    논리 결과

---
+++ src/javaro/Ex_4.java
@@ -0,0 +1,12 @@
+package javaro;
+
+public class Ex_4{
+	public static void main(String[] args) {
+		int x=6,y=7;
+		System.out.println(!(x>6));
+		System.out.println(x==6&&x==5);
+		System.out.println(x==6||x==5);
+		System.out.println(x>-1&&y<10);
+		System.out.println(x>10&&x<5);
+	}
+}
